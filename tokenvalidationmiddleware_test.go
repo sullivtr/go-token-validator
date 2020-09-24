82c2a34db0597ba48860a9d4cd101c389a7d5e57
Khan, Sami <Sami.Khan@tylertech.com>
2020-09-24 15:28:12 -0500

    test: refactor to suite

--- tokenvalidationmiddleware_test.go
+++ tokenvalidationmiddleware_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/base64"
+	"errors"
 	"net/http"
 	"strings"
 	"testing"
@@ -13,7 +14,7 @@ import (
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jws"
 	"github.com/lestrrat-go/jwx/jwt"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 type mockOIDCRequest struct {
@@ -64,51 +65,21 @@ func getModulus(key *rsa.PrivateKey) string {
 	return base64.RawURLEncoding.EncodeToString(key.N.Bytes())
 }
 
-// func TestGetToken(t *testing.T) {
-// 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
-// 	tkn := genToken(key, issuer, false)
-// 	fmt.Println(tkn)
-// }
+type TokenValidationSuite struct {
+	suite.Suite
+}
 
-func TestNew(t *testing.T) {
+func TestTokenValidationSuite(t *testing.T) {
+	suite.Run(t, new(TokenValidationSuite))
+}
+
+func (suite TokenValidationSuite) TestNew() {
 	v := New()
-	assert.NotNil(t, v)
-	assert.NotNil(t, v.Options)
+	suite.NotNil(v)
+	suite.NotNil(v.Options)
 }
 
-func TestNewRSA256ValidatorNoJwks(t *testing.T) {
-	httpmock.ActivateNonDefault(httpClient)
-	defer httpmock.DeactivateAndReset()
-	httpmock.RegisterResponder("GET", jwksURI, httpmock.NewJsonResponderOrPanic(200, Jwks{
-		Keys: []JSONWebKeys{
-			{
-				N:   "bad-mod",
-				E:   "bad-exponent",
-				Kid: kid,
-			},
-		},
-	}))
-
-	v := NewRSA256Validator(&Options{
-		Audience:       audience,
-		Issuer:         issuer,
-		VerifyAudience: true,
-		VerifyIssuer:   true,
-	})
-	assert.NotNil(t, v)
-	req, err := http.NewRequest(http.MethodGet, "http://fake-url-for-test.com", nil)
-	assert.NoError(t, err)
-
-	key, _ := rsa.GenerateKey(rand.Reader, 2048)
-	token := genToken(key, issuer, false)
-
-	req.Header.Add("Authorization", token)
-	tokenIsValid, err := v.ValidateBearerToken(req)
-	assert.Error(t, err)
-	assert.False(t, tokenIsValid)
-}
-
-func TestNewRSA256Validator(t *testing.T) {
+func (suite TokenValidationSuite) TestNewRSA256Validator() {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 
 	cases := []struct {
@@ -122,7 +93,6 @@ func TestNewRSA256Validator(t *testing.T) {
 		{token: genToken(key, "http://wrong-response/", false), issuer: "http://wrong-response/", audience: audience, expectation: false, message: "invalid well-known response should fail to unmarshal response"},
 		{token: genToken(key, "http://bad-jwks-uri/", false), issuer: "http://bad-jwks-uri/", audience: audience, expectation: false, message: "invalid jwksuri response should fail when calling bad url"},
 		{token: genToken(key, "http://bad-jwks-response/", false), issuer: "http://bad-jwks-response/", audience: audience, expectation: false, message: "invalid jwks response should fail when calling unmarshalling jwks"},
-		{token: genToken(key, "http://bad-jwks-response/", false), issuer: "http://bad-jwks-response/", audience: audience, expectation: false, message: "invalid jwks response should fail when calling unmarshalling jwks"},
 		{token: genToken(key, "http://empty-keys/", false), issuer: "http://empty-keys/", audience: audience, expectation: false, message: "empty keys in the jwks response should fail when calling unmarshalling jwks"},
 		{token: genToken(key, "http://genxc5-error/", false), issuer: "http://genxc5-error/", audience: audience, expectation: false, message: "bad keys in jwks should fail when generating public key"},
 		{token: genToken(key, issuer, true), issuer: issuer, audience: audience, expectation: false, message: "expired token should return false"},
@@ -130,6 +100,7 @@ func TestNewRSA256Validator(t *testing.T) {
 		{token: "", issuer: issuer, audience: audience, expectation: false, message: "empty token should return false"},
 		{token: genToken(key, issuer, false), issuer: "invalidissuer", audience: audience, expectation: false, message: "invalidissuer should return false"},
 		{token: genToken(key, issuer, false), issuer: issuer, audience: "invalidaudience", expectation: false, message: "invalidaudience should return false"},
+		{token: genToken(key, "http://error-responder/", false), issuer: "http://error-responder/", audience: audience, expectation: false, message: "bad jwks keys should return false"},
 	}
 
 	httpmock.ActivateNonDefault(httpClient)
@@ -150,6 +121,8 @@ func TestNewRSA256Validator(t *testing.T) {
 	registerWK("http://genxc5-error", OpenIDConfig{JwksURI: "http://genxc5-error"})
 	httpmock.RegisterResponder("GET", "http://genxc5-error", httpmock.NewJsonResponderOrPanic(200, Jwks{Keys: []JSONWebKeys{{N: "-", E: "-", Kid: "unittest"}}}))
 
+	httpmock.RegisterResponder("GET", "http://error-responder/.well-known/openid-configuration", httpmock.NewErrorResponder(errors.New("error")))
+
 	registerWK("http://fake-idp-issuer", OpenIDConfig{JwksURI: "http://fake-idp-issuer"})
 	httpmock.RegisterResponder("GET", "http://fake-idp-issuer", httpmock.NewJsonResponderOrPanic(200, Jwks{
 		Keys: []JSONWebKeys{
@@ -168,17 +141,17 @@ func TestNewRSA256Validator(t *testing.T) {
 			VerifyAudience: true,
 			VerifyIssuer:   true,
 		})
-		assert.NotNil(t, v)
+		suite.NotNil(v)
 		req, err := http.NewRequest(http.MethodGet, "http://fake-url-for-test.com", nil)
-		assert.NoError(t, err)
+		suite.NoError(err)
 
 		req.Header.Add("Authorization", c.token)
 		tokenIsValid, err := v.ValidateBearerToken(req)
-		assert.Equal(t, c.expectation, tokenIsValid, c.message)
+		suite.Equal(c.expectation, tokenIsValid, c.message)
 		if c.expectation {
-			assert.NoError(t, err)
+			suite.NoError(err)
 		} else {
-			assert.Error(t, err)
+			suite.Error(err)
 		}
 	}
 }
@@ -194,7 +167,7 @@ func (d dummySigningMethod) Verify(signingString, signature string, key interfac
 func (d dummySigningMethod) Sign(signingString string, key interface{}) (string, error) {
 	return "", nil
 }
-func TestSigningAlgorithmMismatch(t *testing.T) {
+func (suite TokenValidationSuite) TestSigningAlgorithmMismatch() {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 	m := dummySigningMethod{}
 	v := NewWithOptions(Options{
@@ -205,25 +178,25 @@ func TestSigningAlgorithmMismatch(t *testing.T) {
 	})
 	token := genToken(key, "http://my-fake-issuer", false)
 	req, err := http.NewRequest(http.MethodGet, "http://fake-url-for-test.com", nil)
-	assert.NoError(t, err)
+	suite.NoError(err)
 	req.Header.Add("Authorization", token)
 	valid, err := v.ValidateBearerToken(req)
-	assert.Error(t, err)
-	assert.False(t, valid)
+	suite.Error(err)
+	suite.False(valid)
 }
 
-func TestDefaultValidatorInvalidIssuer(t *testing.T) {
+func (suite TokenValidationSuite) TestDefaultValidatorInvalidIssuer() {
 	token := "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwOi8vdGVzdGlzc3VlciIsImF1ZCI6Im15YXVkIn0.WUHkFhkN2SJy65jyoLjElNPimBXdehXP5_QeFM40Oh1OvrTEbX4dspzEC8RPcDfQYAjtB5XrODFPiNLRaLAUamYVkmBfbKCE3aRHN7gHOxOGDeZ09GDEjGJ59u9YKrQPIRPLURG9Wg3dFF03c48na61X7Ltn9a_6ICUQzqIsE0rLLCEphmARa75KHYt7TpDTcRiUPj4JSW3ivdxvaMMdQTvI3sgumlNb9VAv8tVPH92-s9mkRRnLiJLaN13Pkz3-0cghQXEqRZVUoVaXTDcZrzLu23as_xG5W8Jz2rFZxUmLVDdKN-wK6L6HFIRM5HFMC3CqWY4RDl3sk4bAiH1ouQ"
 	f := GetDefaultValidator(&Options{
 		Audience:       "bad",
 		VerifyAudience: true,
 	})
 	res, err := gojwt.Parse(token, f)
-	assert.False(t, res.Valid)
-	assert.Error(t, err)
+	suite.False(res.Valid)
+	suite.Error(err)
 }
 
-func TestRequestHasScope(t *testing.T) {
+func (suite TokenValidationSuite) TestRequestHasScope() {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 	token := genToken(key, issuer, false)
 	cases := []struct {
@@ -238,15 +211,15 @@ func TestRequestHasScope(t *testing.T) {
 	for _, c := range cases {
 		req, err := http.NewRequest(http.MethodGet, "http://fake-url-for-test.com", nil)
 		if err != nil {
-			assert.Fail(t, "unable to construct httpRequest for unit test")
+			suite.Fail("unable to construct httpRequest for unit test")
 		}
 		req.Header.Add("Authorization", c.token)
 
-		assert.Equal(t, c.expectation, RequestHasScope("testscope", req))
+		suite.Equal(c.expectation, RequestHasScope("testscope", req))
 	}
 }
 
-func TestGetBearerToken(t *testing.T) {
+func (suite TokenValidationSuite) TestGetBearerToken() {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 	token := genToken(key, issuer, false)
 
@@ -263,21 +236,21 @@ func TestGetBearerToken(t *testing.T) {
 	for _, c := range cases {
 		req, err := http.NewRequest(http.MethodGet, "http://fake-url-for-test.com", nil)
 		if err != nil {
-			assert.Fail(t, "unable to construct httpRequest for unit test")
+			suite.Fail("unable to construct httpRequest for unit test")
 		}
 		req.Header.Add("Authorization", c.token)
 
 		token, err := GetBearerToken(req)
-		assert.Equal(t, c.expectation, token)
+		suite.Equal(c.expectation, token)
 		if c.expectedError {
-			assert.Error(t, err)
+			suite.Error(err)
 		} else {
-			assert.NoError(t, err)
+			suite.NoError(err)
 		}
 	}
 }
 
-func TestValidateScope(t *testing.T) {
+func (suite TokenValidationSuite) TestValidateScope() {
 	key, _ := rsa.GenerateKey(rand.Reader, 2048)
 	token := genToken(key, issuer, false)
 	cases := []struct {
@@ -291,11 +264,11 @@ func TestValidateScope(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		assert.Equal(t, c.expectation, ValidateScope(c.testScope, c.token), "expected: %s", c.testScope)
+		suite.Equal(c.expectation, ValidateScope(c.testScope, c.token), "expected: %s", c.testScope)
 	}
 }
 
-func TestGenXC5(t *testing.T) {
+func (suite TokenValidationSuite) TestGenXC5() {
 	cases := []struct {
 		n           string
 		e           string
@@ -308,9 +281,9 @@ func TestGenXC5(t *testing.T) {
 	for _, c := range cases {
 		_, err := genXC5(c.n, c.e)
 		if c.expectError {
-			assert.Error(t, err)
+			suite.Error(err)
 		} else {
-			assert.NoError(t, err)
+			suite.NoError(err)
 		}
 	}
 }
