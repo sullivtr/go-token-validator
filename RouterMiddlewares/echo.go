8e775a28bf04f72f2f5ef5510c9596134958c272
Tyler Sullivan <sullivtr@gmail.com>
2020-09-30 13:58:49 -0700

    feat: add default echo middleware

---
+++ RouterMiddlewares/echo.go
@@ -0,0 +1,21 @@
+package routermiddlewares
+
+import (
+	"github.com/labstack/echo"
+	tvm "github.com/tyler-technologies/go-token-validator"
+)
+
+// Echo middleware for adding bearer token validation into the request pipeline
+func Echo(o tvm.Options) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			v := tvm.NewRSA256Validator(&o)
+			valid, err := v.ValidateBearerToken(c.Request())
+
+			if !valid || err != nil {
+				return echo.ErrUnauthorized
+			}
+			return next(c)
+		}
+	}
+}
